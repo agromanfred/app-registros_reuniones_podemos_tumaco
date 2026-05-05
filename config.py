b83fe8cf6948a437183832e4345d1681b546a7ad
manfred <agromanfred@gmail.com>
2026-05-05 18:17:18 -0500

    fix ssl tidb

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://2yZ2BdEEZzbvjRo.root:Yvy5VzJdfWY9QiNM@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/reuniones_db"
-SQLALCHEMY_ENGINE_OPTIONS = {}
+SQLALCHEMY_ENGINE_OPTIONS = {
+    "connect_args": {
+        "ssl": {
+            "ssl_mode": "VERIFY_IDENTITY"
+        }
+    },
+    "pool_recycle": 280
+}
  
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False

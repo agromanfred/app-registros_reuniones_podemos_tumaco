d458e437bcd6f17997e93d33770489e0e17a68e5
manfred <agromanfred@gmail.com>
2026-05-05 18:11:19 -0500

    fix ssl render

--- config.py
+++ config.py
@@ -1,10 +1,5 @@
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://2yZ2BdEEZzbvjRo.root:Yvy5VzJdfWY9QiNM@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/reuniones_db"
-SQLALCHEMY_ENGINE_OPTIONS = {
-    "connect_args": {
-        "ssl": {
-            "ca": "C:/Users/USUARIO/Downloads/isrgrootx1.pem"
-        }
-    }
-}
+SQLALCHEMY_ENGINE_OPTIONS = {}
+ 
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
